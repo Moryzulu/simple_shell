774b7748fda4cd460e41e116f4425b865cab365a
moryzuluclear <morymacho6@gmail.com>
2022-09-09 13:30:14 -0700

    sinple code to pass bettty check

---
+++ prompt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - void
+ *
+ * Return: void
+ */
+int main(void)
+{
+	char *input;
+	size_t size;
+
+	printf("&");
+	input = (char *) malloc(size);
+	getline(&input, &size, stdin);
+
+	printf("%s", input);
+	return (0);
+}
