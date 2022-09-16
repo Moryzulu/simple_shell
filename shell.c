a0ed11c8c6191be9ed64a317ef0ee584cd541e9f
moryzulu <morymacho6@gmail.com>
2022-09-16 13:38:58 -0700

    my simple shell

---
+++ shell.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+/**
+ * Main - handle prompt
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *input;
+	size_t size;
+
+	while (input != NULL)
+	{
+
+		printf("#ciscool& ");
+		input = (char *) malloc (size);
+		getline(&input, &size, stdin);
+
+		printf("%s", input);
+	}
+	return (0);
+}
+/**
+ * main - fork, wait and execve example
+ *
+ * Return: Always 0.
+ */
+int my_fwe(void)
+{
+	pid_t child_pid = fork();
+	char *args[20];
+
+	if (child_pid == 0) {
+	        execvp(args[0], args);
+        	perror(":not found");
+        	exit(1);
+	}
+       	else if (child_pid > 0) 
+	{
+        	int status;
+        do {
+		waitpid(child_pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}	
+	else 
+	{
+        	perror(":not found");		
+	}
+	return (0);
+
+}
+
