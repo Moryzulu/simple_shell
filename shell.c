dcf5ffba496dfe5dd23001ffc91e5b05b5282fa7
moryzulu <morymacho6@gmail.com>
2022-09-17 11:46:09 -0700

    working on shell

--- shell.c
+++ shell.c
@@ -3,26 +3,33 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+
 /**
  * Main - handle prompt
  * @void: void function
  *
  * Return: Always 0.
  */
+
+char *input;
+
 int main(void)
 {
+
 	char *input;
 	size_t size;
+	
 
 	while (input != NULL)
 	{
-
 		printf("#ciscool& ");
 		input = (char *) malloc(size);
 		getline(&input, &size, stdin);
 
 		printf("%s", input);
 	}
+
 	return (0);
 }
 /**
@@ -34,12 +41,14 @@ int main(void)
 int my_fwe(void)
 {
 	pid_t child_pid = fork();
-	char *args[20];
+	char *argv[] = {"/bin/ls", "-l", "-a", NULL};
 
 	if (child_pid == 0)
 	{
-		execvp(args[0], args);
-		perror(":not found");
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror(":not found");
+		}
 		exit(1);
 	}
 	else if (child_pid > 0)
@@ -56,4 +65,21 @@ int my_fwe(void)
 	return (0);
 
 }
+char **split_input(char *input)
+{
+	char **command = malloc(8 * sizeof(char *));
+        char * separator = " ";
+        int neutral = 0;
+        char *parsed;
+	
+	parsed = strtok(input, separator);
+	while (parsed != NULL)
+        {
+		command[neutral] = parsed;
+		neutral++;
 
+		parsed = strtok(NULL, separator);
+        }
+	command[neutral] = NULL;
+	return (command);
+}
